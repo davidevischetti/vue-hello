33ac38ec05ce78bb74d86e420ed1fefcc138eb54
Davide <davidevischetti@gmail.com>
2022-05-24 16:31:37 +0200

    Esercizio completo + bonus

---
+++ script.js
@@ -0,0 +1,10 @@
+// ISTANZA VUE JS
+const myFirstVueJs = new Vue (
+    {
+        el : '#container',
+        data : {
+            myText : 'Hello Vue.js',
+            myImg : 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'
+        }
+}
+);
